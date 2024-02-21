6f65c5b820dc380dbef036b6b93a319511fda248
Gary Fuller <gary.mark.fuller@gmail.com>
2024-02-21 11:31:49 +0000

    Alter download script to prevent overwriting files

--- download.php
+++ download.php
@@ -4,16 +4,17 @@ $urls = [
     'https://podcasts.files.bbci.co.uk/p02p8xh7.rss',
     'https://podcasts.files.bbci.co.uk/b00729d9.rss'
 ];
-
+$url_no = 0;
 foreach($urls as $url) {
     $xml = simplexml_load_string(file_get_contents($url));
     $json = json_encode($xml);
     $array = json_decode($json,TRUE);
+    $file_no = 0;
     foreach($array['channel']['item'] as $lecture) {
         if ($fp_remote = fopen($lecture['enclosure']['@attributes']['url'], 'rb')) {
 
             // local filename
-            $local_file = 'lectures/' . str_replace(' ', '-', strtolower($lecture['title'])) . '.mp3';
+            $local_file = 'lectures/' . $url_no . '-' . $file_no . '-' . str_replace(' ', '-', strtolower($lecture['title'])) . '.mp3';
         
              // read buffer
             if ($fp_local = fopen($local_file, 'wb')) {
@@ -29,6 +30,8 @@ foreach($urls as $url) {
             }
             // close remote
             fclose($fp_remote);
-        } 
+        }
+        $file_no++; 
     } 
+    $url_no++;
 }
